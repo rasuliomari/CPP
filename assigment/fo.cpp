c646dc84d6780f21c5a78e0101f8cd01243eaee5
rasuliomari <rasuli707046@gmail.com>
2025-12-15 16:51:18 +0300

    new code added

---
+++ assigment/fo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    for(int i=1; i<=5; i++){
+        n=n+i;
+        cout << "The value of n = " << n << endl;
+    }
+
+return 0;
+}
