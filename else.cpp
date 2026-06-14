7645e0dbeba5bcf70e689c988be4e97790872fff
rasuliomari <rasuli707046@gmail.com>
2026-06-14 14:08:56 +0300

    the first else statement

---
+++ else.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int time = 20;
+   if (time < 18) {
+     cout << "Good day.\n";
+   } else {
+     cout << "Good evening.\n";
+}
+// Outputs "Good evening."
+    return 0;
+    }
