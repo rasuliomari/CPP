ed2f3e3d24d6ec360db7b60af7d258de53e3ed33
rasuliomari <rasuli707046@gmail.com>
2026-06-14 14:11:33 +0300

    new element are entered in the element

--- assigment/familia.cpp
+++ assigment/familia.cpp
@@ -2,4 +2,20 @@
 using namespace std;    
 int main() {
     int T25_03_17792[3];
+    cout << "Enter 3 numbers:" << endl;
+    for(int i = 0; i < 3; i++) {
+        cout << "Enter number for index " << i << ": ";
+        cin >> T25_03_17792[i];
+    }
+    cout << "\nYou entered:" << endl;
+    cout <<" ";
+    int i = 0;
+    while (i < 3)
+    {
+        cout<< T25_03_17792[i] << " ";
+        i++;
+    }
+    
+    cout << endl;
+    return 0;
 }
