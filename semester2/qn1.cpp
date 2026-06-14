952c3375d4680fdbea5a86bc8f338969fcfb9156
rasuliomari <rasuli707046@gmail.com>
2026-06-14 14:12:46 +0300

    first assignment of this semester

---
+++ semester2/qn1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+
+    cout << "WELCOME TO THE PROGRAM OF ADDITION OF TWO NUMBERS"<<endl;
+    cout << "Enter firs numbers: ";
+    cin >> a;
+    cout << "Enter second numbers: ";
+    cin >> b;
+
+    sum = a + b;
+
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
