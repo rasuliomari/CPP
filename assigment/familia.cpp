1e17af7a2a826d94bf1959b03adbc8cf15a079b3
rasuliomari <rasuli707046@gmail.com>
2026-06-14 14:11:58 +0300

    new changes to this file

--- assigment/familia.cpp
+++ assigment/familia.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;    
 int main() {
-    int T25_03_17792[3];
-    cout << "Enter 3 numbers:" << endl;
-    for(int i = 0; i < 3; i++) {
+    int T25_03_17792[4];
+    cout << "Enter 4 numbers:" << endl;
+    for(int i = 0; i < 4; i++) {
         cout << "Enter number for index " << i << ": ";
         cin >> T25_03_17792[i];
     }
     cout << "\nYou entered:" << endl;
     cout <<" ";
     int i = 0;
-    while (i < 3)
+    while (i < 4)
     {
         cout<< T25_03_17792[i] << " ";
         i++;
